682228b14d0cb2051b2a75ab8c230d2f0a39271a
savitatekale <110305120+SAVITA987@users.noreply.github.com>
2023-01-28 09:43:17 +0530

    Update PaginationButtonsList.js

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,11 +1,14 @@
 import React from 'react'
-const PaginationButtonsList = () => {
-
+import Button from './Button';
+const PaginationButtonsList = ({page, handlePage}) => {
+    const BtnArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     return (
         <div className="pagination-buttons-list">
-
+            {
+                BtnArr.map(btn => <Button id={`button-${btn}`} key={btn} text={btn} page={page} handlePage={handlePage}/>)
+            }
         </div>
     )
 }
 
-export { PaginationButtonsList }
+export default PaginationButtonsList
