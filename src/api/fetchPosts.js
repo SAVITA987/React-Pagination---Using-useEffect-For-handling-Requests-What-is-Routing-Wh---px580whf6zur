df1901bbbd1bda8f0408da0df83b8fa1f515aaa0
savitatekale <110305120+SAVITA987@users.noreply.github.com>
2023-01-28 09:36:49 +0530

    Update fetchPosts.js

--- src/api/fetchPosts.js
+++ src/api/fetchPosts.js
@@ -1,5 +1,15 @@
 const fetchPosts = async (page, limit) => {
     // optionally write fetching logic here or somewhere else
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`;
+    try{
+        const response = await fetch(API_URL);
+        if(!response.ok) throw Error("Wrong Url");
+        const data = await response.json();
+        return data;
+    }catch(err){
+        console.log(err);
+    }
 }
+
 
 export { fetchPosts }
