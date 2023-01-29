a9cd066a7158ec312ade2fb8aef8a7d5501435a0
savitatekale <110305120+SAVITA987@users.noreply.github.com>
2023-01-29 22:09:34 +0530

    Update PostList.js

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,48 @@
-import React from 'react'
-const Post = ({body, title}) => {
-    return (
-        <div className="post">
-            <h3 className='title'>{title}</h3>
-            <p className='body'>{body}</p>
-        </div>
-    )
-}
+import React, { useEffect, useState } from "react";
+import { fetchPosts } from "../api/fetchPosts";
+import { PaginationButtonsList } from "./PaginationButtonsList";
+import { Post } from "./Post";
 
-export default Post
+const PostList = () => {
+  const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const loadData = async () => {
+    fetchPosts(page, 5)
+      .then((res) => res.json())
+      .then((jsonData) => {
+        setData(jsonData);
+      });
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    setData(null);
+    loadData();
+  }, [page]);
+
+  const clickHandler = (val) => {
+    setPage(val);
+  };
+
+  return (
+    <>
+      {data == null ? (
+        <div id="loader" className="loader">
+          loading
+        </div>
+      ) : (
+        data.map((ele) => {
+          return <Post ele={ele} key={ele.id} />;
+        })
+      )}
+      <PaginationButtonsList page={page} clickHandler={clickHandler} />
+    </>
+  );
+};
+
+export { PostList };
+Footer
